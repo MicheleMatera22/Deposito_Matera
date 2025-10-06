955821f87bbef2d65ed795e68d9063fd90112a29
MicheleMatera22 <michele.matera1234@gmail.com>
2025-10-06 15:50:39 +0200

    Upload esercizio Classe Studente

---
+++ 06_10_25/Pomeriggio/Studente/Program.cs
@@ -0,0 +1,29 @@
+﻿class Studente
+{
+    public string Nome;
+    public int Matricola;
+    public double MediaVoti;
+
+    public Studente(string Nome, int Matricola, double MediaVoti)
+    {
+        this.Nome = Nome;
+        this.Matricola = Matricola;
+        this.MediaVoti = MediaVoti;
+    }
+
+    public void stampaStudente()
+    {
+        Console.WriteLine($"Nome: {Nome}, Matricola:{Matricola}, Media voti: {MediaVoti}");
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Studente s1 = new Studente("Giorgio", 1, 8.5);
+            Studente s2 = new Studente("Samuele", 2, 6.4);
+            s1.stampaStudente();
+            s2.stampaStudente();
+        }
+    }
+}
