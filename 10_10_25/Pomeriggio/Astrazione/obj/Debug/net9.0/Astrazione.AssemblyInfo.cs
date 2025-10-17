f47ea0cb1c4d0ae686ae5e1615cf3a9594eb9c6f
MicheleMatera22 <michele.matera1234@gmail.com>
2025-10-17 10:20:11 +0200

    a

--- 10_10_25/Pomeriggio/Astrazione/obj/Debug/net9.0/Astrazione.AssemblyInfo.cs
+++ 10_10_25/Pomeriggio/Astrazione/obj/Debug/net9.0/Astrazione.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Astrazione")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a9b4fc76535637e805673c31c303f9fe72ff4ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+238d789892223515eb6563cd0210c04330ee2d22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Astrazione")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Astrazione")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
